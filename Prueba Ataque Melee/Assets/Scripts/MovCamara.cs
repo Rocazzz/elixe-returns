f97763cfc9d68ff6bfabaf72439d98db628b93c4
Rocazzz <camiloakarck@gmail.com>
2023-04-16 11:55:36 -0500

    Arreglo De Camara y Escenas

--- Prueba Ataque Melee/Assets/Scripts/MovCamara.cs
+++ Prueba Ataque Melee/Assets/Scripts/MovCamara.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class MovCamara : MonoBehaviour
 {
     public Transform target; // el objeto que la cámara seguirá
-    public float smoothSpeed = 0.125f; // la suavidad del seguimiento
+    public float smoothSpeed; // la suavidad del seguimiento
 
     void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + new Vector3(0, 0, -10); // la posición deseada de la cámara
+        Vector3 desiredPosition = target.position + new Vector3(0f, 0.6f, -40f); // la posición deseada de la cámara
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed); // la posición suavizada de la cámara
         transform.position = smoothedPosition; // actualiza la posición de la cámara
     }
