a9230c9ee7068c0f35290f2f57913b5528140520
juanmayorquin <127550805+juanmayorquin@users.noreply.github.com>
2023-04-18 16:34:07 -0500

    Sistema de batallas completado
    Gracias mamá, gracias papá, profe pirobo .l.

--- Prueba Ataque Melee/Assets/Scripts/Enemigo.cs
+++ Prueba Ataque Melee/Assets/Scripts/Enemigo.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.iOS;
 using UnityEngine.SceneManagement;
 
 public class Enemigo : Personaje
 {
+    [SerializeField] private int cantPociones, cantCofres, cantOro;
+    [SerializeField] private bool sueltaEscapulario;
+
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         foreach (Heroe hero in FindFirstObjectByType<PlayersMovement>().GetComponentsInChildren<Heroe>())
         {
             rivales.Add(hero);
         }
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -24,8 +28,25 @@ public class Enemigo : Personaje
     {
         if (collision.collider.CompareTag("Player"))
         {
-            DontDestroyOnLoad(gameObject);
+            AlmacenarEnemigo.AlmacenarEsteEnemigo(gameObject);
             SceneManager.LoadScene("Batalla");
+            Destroy(gameObject);
         }
+    }
+
+    public void SoltarDrop()
+    {
+        PlayersMovement jugador = FindObjectOfType<PlayersMovement>();
+
+        jugador.cantidadPociones += cantPociones;            
+        jugador.cantidadCofres += cantCofres;        
+
+        jugador.dinero += cantOro;
+
+        if (sueltaEscapulario == true)
+        {
+            jugador.inventario.Add("Escapulario de Elixe", 1);
+        }
+        
     }
 }
