34050a18e5faa92a19ae78cdce74fef895d08fe2
juanmayorquin <127550805+juanmayorquin@users.noreply.github.com>
2023-04-19 01:31:05 -0500

    Escenas de batalla para todos los personajes

--- Prueba Ataque Melee/Assets/Scripts/UI.cs
+++ Prueba Ataque Melee/Assets/Scripts/UI.cs
@@ -5,12 +5,13 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
-    public Text cantMonedas, cantPociones, escapulario;
+    public Text cantMonedas, cantPociones, cantCofres, escapulario;
 
     private void Update()
     {
         cantMonedas.text = Inventario.dinero.ToString("00");
         cantPociones.text = Inventario.cantPociones.ToString("00");
+        cantCofres.text = Inventario.cantCofres.ToString("00");
         escapulario.text = Inventario.escapulario.ToString("00");
     }
 }
