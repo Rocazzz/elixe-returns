2299d3796c28d54ee8f8899990d6c9c7e7a30c9c
Rocazzz <camiloakarck@gmail.com>
2023-04-19 13:10:33 -0500

    Animaciones

--- Prueba Ataque Melee/Assets/Scripts/Transicion.cs
+++ Prueba Ataque Melee/Assets/Scripts/Transicion.cs
@@ -5,36 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Transicion : MonoBehaviour
 {
-    //public string scene;
-    public Vector2 playerPosition;
-    public VectorValue playerStorage;
-    public Animator transition;
-    public float transitionTime = 1f;
+ 
+     public string scene;
+     public Vector2 playerPosition;
+     public VectorValue playerStorage;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player") && !collision.isTrigger)
         {
             playerStorage.initialValue = playerPosition;
-            LoadNextLevel();
-
-
+            SceneManager.LoadScene(scene);
         }
-        
     }
-    public void LoadNextLevel()
-    {
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
-    
-    }
-
-      IEnumerator LoadLevel(int levelIndex)
-        {
-            transition.SetTrigger("Start");
-
-            yield return new WaitForSeconds(transitionTime);
-
-            SceneManager.LoadScene(levelIndex);
-            Debug.Log ("dw");
-        }
+ 
 }
+
